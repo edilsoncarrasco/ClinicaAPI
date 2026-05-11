f5d556830b6c2a53d25dc0c67090c0cb1c477bb4
Edilson <021200573k@uandina.edu.pe>
2026-05-11 15:45:15 -0500

    Implementación CI con GitHub Actions

--- obj/Debug/net10.0/ClinicaAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/ClinicaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bfc8c98f5338b15a08be65190f9c79e38e372d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
