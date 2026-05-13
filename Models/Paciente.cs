572c7c33564aed30f1bfe854a8f2d87390361334
Edilson <021200573k@uandina.edu.pe>
2026-05-13 15:10:08 -0500

    Integración de pruebas unitarias con xUnit

--- Models/Paciente.cs
+++ Models/Paciente.cs
@@ -4,10 +4,18 @@
     {
         public int Id { get; set; }
 
-        public string Nombre { get; set; }
+        public string Nombre { get; set; } = string.Empty;
 
         public int Edad { get; set; }
 
-        public string Diagnostico { get; set; }
+        public string Diagnostico { get; set; } = string.Empty;
+
+        public string Telefono { get; set; } = string.Empty;
+
+        public string Direccion { get; set; } = string.Empty;
+
+        public string CorreoElectronico { get; set; } = string.Empty;
+
+        public DateTime FechaRegistro { get; set; }
     }
 }
